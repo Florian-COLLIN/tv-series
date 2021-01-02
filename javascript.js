587b562468216ceabcf65464c1a17c52ac56ed4c
Florian-COLLIN <73430474+Florian-COLLIN@users.noreply.github.com>
2021-01-02 23:09:33 +0100

    Test

--- javascript.js
+++
@@ -1,3 +0,0 @@
-function rech(form1) {
-	document.location.href="https://www.google.com/search?q=" + document.form1.rec.value + "&sitesearch=https://florian.collin.github.io/tv-series/&meta=lr%3Dlang_fr"
-}
