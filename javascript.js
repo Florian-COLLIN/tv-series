fbc0e7218d43a8916ddf83f426c446a884d05f92
Florian-COLLIN <73430474+Florian-COLLIN@users.noreply.github.com>
2021-01-10 19:34:09 +0100

    Added function .getFullYear

---
+++ javascript.js
@@ -0,0 +1,11 @@
+/* @author : Florian COLLIN
+ * 
+ * @date : 2021
+ * 
+ * @next-revision : 2021
+ */
+/* Affichage de la date en temps réel */
+var today = new Date();
+var annee = today.getFullYear();
+var sannee = document.getElementById('s_annee');
+sannee.innerHTML = annee;
