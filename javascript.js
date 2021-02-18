70e23d71045dcf17a828a6c2495e440a00287798
Florian-COLLIN <73430474+Florian-COLLIN@users.noreply.github.com>
2021-02-18 09:51:50 +0100

    Onglets

--- javascript.js
+++ javascript.js
@@ -17,6 +17,18 @@ function affiche_annee() {
 }
 /* </pre>
 
+== Onglets ==
+
+<pre> */
+function change_onglet(name) {
+	document.getElementById('onglet_'+anc_onglet).className = 'onglet_0 onglet';
+	document.getElementById('onglet_'+name).className = 'onglet_1 onglet';
+	document.getElementById('contenu_onglet_'+anc_onglet).style.display = 'none';
+	document.getElementById('contenu_onglet_'+name).style.display = 'block';
+	anc_onglet = name;
+}
+/* </pre>
+
 == Fin du code ==
 
 */
