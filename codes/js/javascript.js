2bdbe7c73afc31dffd6c1a06eb48f41c306798c9
Florian-COLLIN <73430474+Florian-COLLIN@users.noreply.github.com>
2021-03-08 21:05:09 +0100

    Update javascript.js

--- codes/js/javascript.js
+++ codes/js/javascript.js
@@ -21,10 +21,10 @@ function affiche_annee() {
 
 <pre> */
 function change_onglet(name) {
-	document.getElementById('onglet_'+anc_onglet).className = 'onglet_0 onglet';
-	document.getElementById('onglet_'+name).className = 'onglet_1 onglet';
-	document.getElementById('contenu_onglet_'+anc_onglet).style.display = 'none';
-	document.getElementById('contenu_onglet_'+name).style.display = 'block';
+	document.getElementById("onglet_" + anc_onglet).className = "onglet_0 onglet";
+	document.getElementById("onglet_" + name).className = "onglet_1 onglet";
+	document.getElementById("contenu_onglet_" + anc_onglet).style.display = "none";
+	document.getElementById("contenu_onglet_" + name).style.display = "block";
 	anc_onglet = name;
 }
 /* </pre>
