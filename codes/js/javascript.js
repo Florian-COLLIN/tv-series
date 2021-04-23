7b9690397dcd28e814497e98cd96ac786705d750
Florian-COLLIN <73430474+Florian-COLLIN@users.noreply.github.com>
2021-04-23 14:11:05 +0200

    Update

--- codes/js/javascript.js
+++ codes/js/javascript.js
@@ -45,14 +45,21 @@ function miseajour() {
 }
 /* </pre>
 
-== Lecteur vidéo ==
+== Retour en haut ==
 
 <pre> */
-document.addEventListener('DOMContentLoaded', function() {
-	window.onscroll = function(ev) {
-		document.getElementById("cRetour").className = (window.pageYOffset > 100) ? "cVisible" : "cInvisible";
-	};
+jQuery(function(){
+    $(function () {
+        $(window).scroll(function () { //Fonction appelée quand on descend la page
+            if ($(this).scrollTop() > 200 ) {  // Quand on est à 200pixels du haut de page,
+                $('#scrollUp').css('right','10px'); // Replace à 10pixels de la droite l'image
+            } else {
+                $('#scrollUp').removeAttr( 'style' ); // Enlève les attributs CSS affectés par javascript
+            }
+        });
+    });
 });
+
 /* </pre>
 
 == Lecteur vidéo ==
