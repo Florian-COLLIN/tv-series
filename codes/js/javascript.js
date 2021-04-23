a4615ab273825ef733b3222f8e97b53502bb7894
Florian-COLLIN <73430474+Florian-COLLIN@users.noreply.github.com>
2021-04-23 14:16:54 +0200

    Undo

--- codes/js/javascript.js
+++ codes/js/javascript.js
@@ -48,16 +48,10 @@ function miseajour() {
 == Retour en haut ==
 
 <pre> */
-jQuery(function(){
-    $(function () {
-        $(window).scroll(function () { //Fonction appelée quand on descend la page
-            if ($(this).scrollTop() > 200 ) {  // Quand on est à 200pixels du haut de page,
-                $('#scrollUp').css('right','10px'); // Replace à 10pixels de la droite l'image
-            } else {
-                $('#scrollUp').removeAttr( 'style' ); // Enlève les attributs CSS affectés par javascript
-            }
-        });
-    });
+document.addEventListener('DOMContentLoaded', function() {
+	window.onscroll = function(ev) {
+		document.getElementById("cRetour").className = (window.pageYOffset > 100) ? "cVisible" : "cInvisible";
+	};
 });
 
 /* </pre>
