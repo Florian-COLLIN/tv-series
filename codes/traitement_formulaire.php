44597c7a73d4e6c14059b20fe0b1b55bf1b6f39c
Florian-COLLIN <73430474+Florian-COLLIN@users.noreply.github.com>
2021-04-25 17:18:06 +0200

    Update traitement_formulaire.php

--- codes/traitement_formulaire.php
+++ codes/traitement_formulaire.php
@@ -1,17 +1,17 @@
 <?php
 $email_expediteur = 'florian.collin83@gmail.com';
 $nom_expediteur = 'Contact tv-series.com';
- 
+
 $destinataire = 'florian.collin36@gmail.com';
 
 $copie = 'oui';
- 
+
 $message_envoye = "Votre message nous est bien parvenu !";
 $message_non_envoye = "L'envoi du mail a échoué, veuillez réessayer SVP.";
- 
+
 $message_erreur_formulaire = "Vous devez d'abord <a href=\"/tv-series/contact.html\">envoyer le formulaire</a>.";
 $message_formulaire_invalide = "Vérifiez que tous les champs soient bien remplis et que l'email soit sans erreur.";
- 
+
 // on teste si le formulaire a été soumis
 if (!isset($_POST['envoi']))
 {
@@ -30,11 +30,11 @@ else
 		{
 			$text = stripslashes($text);
 		}
- 
+
 		$text = nl2br($text);
 		return $text;
 	};
- 
+
 	/*
 	 * Cette fonction sert à vérifier la syntaxe d'un email
 	 */
@@ -43,16 +43,16 @@ else
 		$value = preg_match('/^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9_](?:[a-zA-Z0-9_\-](?!\.)){0,61}[a-zA-Z0-9_-]?\.)+[a-zA-Z0-9_](?:[a-zA-Z0-9_\-](?!$)){0,61}[a-zA-Z0-9_]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/', $email);
 		return (($value === 0) || ($value === false)) ? false : true;
 	}
- 
+
 	// formulaire envoyé, on récupère tous les champs.
 	$nom     = (isset($_POST['nom']))     ? Rec($_POST['nom'])     : '';
 	$email   = (isset($_POST['email']))   ? Rec($_POST['email'])   : '';
 	$objet   = (isset($_POST['objet']))   ? Rec($_POST['objet'])   : '';
 	$message = (isset($_POST['message'])) ? Rec($_POST['message']) : '';
- 
+
 	// On va vérifier les variables et l'email ...
 	$email = (IsEmail($email)) ? $email : ''; // soit l'email est vide si erroné, soit il vaut l'email entré
- 
+
 	if (($nom != '') && ($email != '') && ($objet != '') && ($message != ''))
 	{
 		// les 4 variables sont remplies, on génère puis envoie le mail
@@ -63,7 +63,7 @@ else
 				'Content-Disposition: inline'. "\r\n" .
 				'Content-Transfer-Encoding: 7bit'." \r\n" .
 				'X-Mailer:PHP/'.phpversion();
- 
+
 		// Envoyer une copie au visiteur ?
 		if ($copie == 'oui')
 		{
@@ -73,17 +73,17 @@ else
 		{
 			$cible = $destinataire;
 		};
- 
+
 		// Remplacement de certains caractères spéciaux
 		$caracteres_speciaux     = array('&#039;', '&#8217;', '&quot;', '<br>', '<br />', '&lt;', '&gt;', '&amp;', '…',   '&rsquo;', '&lsquo;');
 		$caracteres_remplacement = array("'",      "'",        '"',      '',    '',       '<',    '>',    '&',     '...', '>>',      '<<'     );
- 
+
 		$objet = html_entity_decode($objet);
 		$objet = str_replace($caracteres_speciaux, $caracteres_remplacement, $objet);
- 
+
 		$message = html_entity_decode($message);
 		$message = str_replace($caracteres_speciaux, $caracteres_remplacement, $message);
- 
+
 		// Envoi du mail
 		$cible = str_replace(',', ';', $cible); // antibug : j'ai vu plein de forums où ce script était mis, les gens ne font pas attention à ce détail parfois
 		$num_emails = 0;
@@ -93,7 +93,7 @@ else
 			if (mail($email_destinataire, $objet, $message, $headers))
 				$num_emails++;
 		}
- 
+
 		if ((($copie == 'oui') && ($num_emails == 2)) || (($copie == 'non') && ($num_emails == 1)))
 		{
 			echo '<p>'.$message_envoye.'</p>';
