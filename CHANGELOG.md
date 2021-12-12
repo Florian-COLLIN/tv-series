4408060724cb819d9a718cb1650caa4d2f0c569b
Florian COLLIN <florian.collin83@gmail.com>
2021-12-12 18:12:48 +0100

    Update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -12,6 +12,10 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 
 ### Fixed
 
+## [1.1.2] - 2021-12-12
+### Changed
+- Mobile site version.
+
 ## [1.1.1] - 2021-12-04
 ### Fixed
 - JavaScript display bug fixed on ifsd.netlify.app.
--- codes/js/javascript.js
+++ codes/js/javascript.js
@@ -21,7 +21,7 @@ function affiche_annee() {
 
 <pre> */
 function version_ifsd() {
-	var version_ifsd = "1.1.1";
+	var version_ifsd = "1.1.2";
 	var link = "https://github.com/Florian-COLLIN/tv-series/releases/tag/v"
 	window.document.write("(<a href=\"" + link + version_ifsd + "\">v." + version_ifsd + "</a>)");
 }
